b8cece556af8cd8562ac63094f99df00e64cdffc
RomainC75 <rom.chenard@gmail.com>
2023-03-09 10:22:31 +0100

    added 1 script in package.json to use ts-node-dev

--- front/src/utils/common.ts
+++ front/src/utils/common.ts
@@ -1,13 +1,11 @@
 export const extractSimpleDate = (strDate:string):string =>{
     const date= new Date(strDate)
-    console.log('extract simple date : ', date)
     return `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2,'0')}-${date.getDate().toString().padStart(2,'0')}`
 }
 
 export const getRealYYYMMDD = (date:string):string =>{
     const parts:string[]=date.split("-")
     parts[1]=(parseInt(parts[1])+1).toString().padStart(2,'0')
-    console.log("parts", parts)
     return parts.join("-")
 }
 
