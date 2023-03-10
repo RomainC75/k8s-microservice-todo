43769c696011f8ab20f21483d8e94bc36e563493
RomainC75 <rom.chenard@gmail.com>
2023-03-10 13:36:02 +0100

    simplified the www.ts file - use of app.listen

--- back/src/bin/www.ts
+++ back/src/bin/www.ts
@@ -1,23 +1,13 @@
 #!/usr/bin/env node
 
-
 import app from '../app'
-const debug = require('debug')('back:server');
-import http from 'http'
 import dotenv from 'dotenv'
 dotenv.config()
 require('../db/index')
 
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
-
-const server = http.createServer(app);
-
-
-server.listen(port,()=>console.log(`==>listening on port : ${port}`));
-server.on('error', onError);
-server.on('listening', onListening);
-
+app.listen(port,()=>console.log(`==>listening on port : ${port}`))
 
 function normalizePort(val) {
   const port = parseInt(val, 10);
@@ -25,38 +15,7 @@ function normalizePort(val) {
     return val;
   }
   if (port >= 0) {
-    return port;
+      return port;
   }
   return false;
-}
-
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
 }
